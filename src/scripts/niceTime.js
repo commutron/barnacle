9e1f28d31509e6b6240037a94a8b1f77b0166347
Matthew Andreas <andreas.mjp@gmail.com>
2021-08-12 22:10:25 -0600

    am pm display fix

--- src/scripts/niceTime.js
+++ src/scripts/niceTime.js
@@ -17,7 +17,7 @@ export function niceTime(hr, min) {
     const twelveHr = numHr === 0 ? 12 : numHr > 12 ? numHr - 12 : numHr;
     const strHr = twelveHr.toString();
 
-    const stAp = numHr > 12 ? 'p.m.' : 'a.m.';
+    const stAp = numHr >= 12 ? 'p.m.' : 'a.m.';
 
     const timeStr = `${strHr}:${strMn} ${stAp}`;
 
